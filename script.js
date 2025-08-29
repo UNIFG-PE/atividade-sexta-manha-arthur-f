c76e430f9e43fbf853adf30a004560bbe5f95a4d
Memz-idk <arthurdefigueiredo@hotmail.com>
2025-08-29 11:28:01 -0300

    Add exercises 3 through 6

--- script.js
+++ script.js
@@ -54,3 +54,57 @@
             console.error("operador inválido");
     }
 });
+
+/** @type {HTMLButtonElement} */ (document.getElementById("botao-tabuada")).addEventListener("click", () => {
+    const num = parseFloat(prompt("Número:") ?? "");
+    if(Number.isNaN(num)) {
+        console.error("inválido");
+        return;
+    }
+    for(let i = 1; i <=10; ++i) {
+        console.log(`${num}*${i}=${num*i}`);
+    }
+});
+
+/** @type {HTMLButtonElement} */ (document.getElementById("botao-adv")).addEventListener("click", () => {
+    const num = Math.floor(Math.random()*10)+1;
+    let guess = -1;
+    while(true) {
+        guess = parseInt(prompt("Advinhar número") ?? "");
+        if(guess === num) {
+            alert("ACERTO");
+            break;
+        }else if(Number.isNaN(guess)) {
+            alert("INVÁLIDO");
+        }else if(guess < num) {
+            alert("MUITO BAIXO");
+        }else if(guess > num) {
+            alert("MUITO ALTO");
+        }
+    }
+});
+
+/** @type {HTMLButtonElement} */ (document.getElementById("botao-temp")).addEventListener("click", () => {
+    const num = parseFloat(prompt("Temperatura:") ?? "");
+    const convType = /** @type {HTMLSelectElement} */ (document.getElementById("select-temp")).value;
+    if(convType === "ctf") {
+        alert(`Resultado: ${(num*9/5)+32}°F`);
+    }else if(convType === "ftc") {
+        alert(`Resultado: ${(num-32)*5/9}°C`);
+    }
+});
+
+/** @type {HTMLButtonElement} */ (document.getElementById("botao-tarefas")).addEventListener("click", () => {
+    const /** @type {string[]} */ tasks = [];
+    let task = "";
+    while(true) {
+        task = prompt('Adicionar tarafa? (Digite "não" ou "sair" para parar de adicionar)') ?? "";
+        if(task === "não" || task === "sair") {
+            break;
+        }else {
+            tasks.push(task);
+        }
+    }
+    /** @type {HTMLParagraphElement} */ (document.getElementById("tarefas")).innerHTML = 
+        `TAREFAS:<br>${tasks.join("<br>")}<br>=====`;
+});
